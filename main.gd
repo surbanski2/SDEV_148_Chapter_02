1f19faac12d9dcd90680e6ecabcadd395e1af539
Scott Urbanski <scotturbanski@Scotts-MacBook-Air.local>
2025-03-24 15:04:00 -0500

    Finished game.

--- main.gd
+++ main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var coin_scene : PackedScene
+@export var powerup_scene : PackedScene
 @export var playtime = 30
 
 var level = 1
@@ -27,24 +28,29 @@ func new_game():
 	$HUD.update_timer(time_left)
 	
 func spawn_coins():
+	$LevelSound.play()
 	for i in level + 4:
 		var c = coin_scene.instantiate()
 		add_child(c)
 		c.screensize = screensize
 		c.position = Vector2(randi_range(0, screensize.x), randi_range(0, screensize.y))
+		
 
 func _process(delta):
 	if playing and get_tree().get_nodes_in_group("coins").size() == 0:
 		level += 1
 		time_left += 5
 		spawn_coins()
-	
+		$PowerupTimer.start()
+		
+		
 func game_over():
 	playing = false
 	$GameTimer.stop()
 	get_tree().call_group("coins", "queue_free")
 	$HUD.show_game_over()
 	$Player.die()
+	$EndSound.play()
 
 func _on_hud_start_game():
 	new_game()
@@ -59,8 +65,21 @@ func _on_player_hurt():
 	game_over()
 	
 
+func _on_player_pickup(type):
+	match type:
+		"coin":
+			score += 1
+			$HUD.update_score(score)
+			$CoinSound.play()
+		"powerup":
+			$PowerupSound.play()
+			time_left += 5
+			$HUD.update_timer(time_left)
 
 
-func _on_player_pickup():
-	score += 1
-	$HUD.update_score(score)
+
+func _on_powerup_timer_timeout():
+	var p = powerup_scene.instantiate()
+	add_child(p)
+	p.screensize = screensize
+	p.position = Vector2(randi_range(0, screensize.x), randi_range(0,screensize.y))
--- powerup.gd
+++ powerup.gd
@@ -9,15 +9,11 @@ func pickup():
 	tw.tween_property(self, "modulate:a", 0.0, 0.3)
 	await tw.finished
 	queue_free()
-	
-func  _ready():
-	$Timer.start(randf_range(3,8))
-	
 
 
-func _on_timer_timeout():
-	$AnimatedSprite2D.frame = 0
-	$AnimatedSprite2D.play()
+func _on_lifetime_timeout():
+	queue_free()
+
 
 
 func _on_area_entered(area):
